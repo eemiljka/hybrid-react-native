4a44b2e1bbd46f676db76efdc4c1d28994bbbf51
eemiljka <eemiljka@metropolia.fi>
2024-02-29 12:46:13 +0200

    comp-lib

--- App.tsx
+++ App.tsx
@@ -1,25 +1,14 @@
 import {StatusBar} from 'expo-status-bar';
-import {Platform, SafeAreaView, StyleSheet} from 'react-native';
-import Home from './src/views/Home';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigator from './src/navigators/Navigator';
 
 const App = () => {
   return (
-    <>
-      <Navigator></Navigator>
+    <SafeAreaProvider>
+      <Navigator />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
--- src/views/Single.tsx
+++ src/views/Single.tsx
@@ -1,30 +1,46 @@
-import {Image, Text, View} from 'react-native';
-import {ResizeMode, Video} from 'expo-av';
+import {Card, Text, ListItem, Icon} from '@rneui/themed';
+import {Video, ResizeMode} from 'expo-av';
 import {MediaItemWithOwner} from '../types/DBTypes';
 
 const Single = ({route}: any) => {
   const item: MediaItemWithOwner = route.params;
   const [fileType, fileFormat] = item.media_type.split('&#x2F;');
+
   return (
-    <View>
-      <Text>{item.title}</Text>
+    <Card>
+      <Card.Title>{item.title}</Card.Title>
       {fileType === 'image' ? (
-        <Image style={{height: 500}} source={{uri: 'http:' + item.filename}} />
+        <Card.Image
+          style={{height: 400}}
+          resizeMode="contain"
+          source={{uri: 'http:' + item.filename}}
+        />
       ) : (
         <Video
-          style={{height: 500}}
+          style={{height: 400}}
           source={{uri: 'http:' + item.filename}}
           useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
         />
       )}
-      <Text>{item.description}</Text>
-      <Text>{new Date(item.created_at).toLocaleString('fi-FI')}</Text>
-      <Text>Owner: {item.username}</Text>
-      <Text>
-        Media type: {fileType} / {fileFormat}
-      </Text>
-      <Text>File size: {Math.round(item.filesize / 1024)} kB</Text>
-    </View>
+      <ListItem>
+        <Text>{item.description}</Text>
+      </ListItem>
+      <ListItem>
+        <Icon name="today" />
+        <Text>{new Date(item.created_at).toLocaleString('fi-FI')}</Text>
+      </ListItem>
+      <ListItem>
+        <Icon name="person" />
+        <Text>{item.username}</Text>
+      </ListItem>
+      <ListItem>
+        <Icon name="image" />
+        <Text>
+          {fileType} / {fileFormat}, {Math.round(item.filesize / 1024)} kB
+        </Text>
+      </ListItem>
+    </Card>
   );
 };
 
